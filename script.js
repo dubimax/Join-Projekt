2fd772440aac1da7dcd05b112e718011df0b292a
Marijan Dupkovic <mdu@dupko-it.de>
2023-04-05 17:08:40 +0200

    Updated Nav

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
+
+
 function init(){
     generateAddTaskHTML()
 }
 
 function generateAddTaskHTML(){
     addContentTitle();
-    generateInputHeadline('Title');
-    generateInputHeadline('Description');
-    generateInputHeadline('Category');
-    generateInputHeadline('Assigned To');
+    generateTableHTML();
+
+    generateTableRowHTML('Title');
+    generateTableRowHTML('Description');
+    generateTableRowHTML('Category');
+    generateTableRowHTML('Assigned To');
 
 }
 
 function generateTableHTML(){
-    document.getElementById('content').innerHTML += `
+    document.getElementById('addTask').innerHTML += `
     <table>
         <tbody id="addTaskDetails"></tbody>
     </table>
     `;
-    generateTableRowHTML();
 }
 
 function addContentTitle(){
-    document.getElementById('content').innerHTML += `
+    document.getElementById('addTask').innerHTML += `
     <h2>Add Task</h2>
     `;
 }
 
-function generateTableRowHTML(){
-    document.getElementById('addTaskDetails').innerHTML += `<tr><td id="detail"></td></tr>`;
+function generateTableRowHTML(headline){
+    document.getElementById('addTaskDetails').innerHTML += `<tr id="detail"><td>${headline}</td></tr>`;
 
 }
 
 function generateInputHeadline(headline){
-    generateTableHTML();
-    document.getElementById('detail').innerHTML += headline;
+
+    document.getElementById('detail').innerHTML += ``;
 }
