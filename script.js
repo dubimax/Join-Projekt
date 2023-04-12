9017ba5e41091fcd5a6bed73ff5b9ac5466d6dab
Marijan Dupkovic <mdu@dupko-it.de>
2023-04-12 09:46:48 +0200

    Changed some html functions

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 
+let urgentColor = 'background-color: #FF3D00; color: #FFFFFF;';
+let mediumColor = 'background-color: #FFA800;color: #FFFFFF;';
+let lowColor = 'background-color: #7AE229;color: #FFFFFF;';
 
 
+function changeStyleOfLabel(id) {
+    let activeID = '';
+    let activeImg = '';
+    document.getElementById('id_urgent').style = 'background-color: #FFFFFF; color: #000000;';
+    document.getElementById('id_medium').style = 'background-color: #FFFFFF; color: #000000;';
+    document.getElementById('id_low').style = 'background-color: #FFFFFF; color: #000000;';
+    document.getElementById('urgentImgID').src = 'img/urgentIcon.png';
+    document.getElementById('mediumImgID').src = 'img/mediumIcon.png';
+    document.getElementById('lowImgID').src = 'img/lowIcon.png';
+    if(activeID.length <=0){
+        if (id == 'id_urgent') {
+            document.getElementById(id).style = urgentColor;
+            document.getElementById('urgentImgID').src = 'img/urgentWhiteIcon.png';
+            activeID = id;
+            activeImg = 'urgentImgID';
+            return;
+        }
+        if (id == 'id_medium') {
+            document.getElementById(id).style = mediumColor;
+            document.getElementById('mediumImgID').src = 'img/mediumWhiteIcon.png';
+            activeID = id;
+            activeImg = 'mediumImgID';
+            return;
+        }
+        if (id == 'id_low') {
+            document.getElementById(id).style = lowColor;
+            document.getElementById('lowImgID').src = 'img/lowWhiteIcon.png';
+            activeID = id;
+            activeImg = 'mediumImgID';
+            return;
+        }
+    }else {
+        document.getElementById(activeID).style = 'background-color: #FFFFFF; color: #000000;';
+        document.getElementById(activeImg).style = 'background-color: #FFFFFF; color: #000000;';
+        activeID = '';
+        activeImg = '';
+    }
+   
+}
 /**
  *  Generates Add Task HTML Content
  */
@@ -11,14 +53,14 @@ function generateAddTaskHTML() {
         <div class="detailBox-left">
             ${generatesInputFieldHTML('label', 'input', 'Title', 'inputTextStd', 'text', 'inputTitle')}
             ${generatesTextareaFieldHTML('label', 'textarea', 'Description')}
-            ${generatesOptionsFieldHTML('label', 'select', 'Category', 'dropDownMenuField', 'categoryBox')}
+            ${generatesOptionsFieldHTML('label', 'select', 'Category', 'dropDownMenuField', 'categoryBox', 'categories')}
 
-            ${generatesOptionsFieldHTML('label', 'select', 'Assigned to', 'dropDownMenuField', 'assignedTo')}
+            ${generatesOptionsFieldHTML('label', 'select', 'Assigned to', 'dropDownMenuField', 'assignedTo', 'users')}
 
         </div>
         <div class="border-1px-solid"></div>
         <div class="detailBox-right">
-        ${generatesInputFieldHTML('label', 'input', 'Due Date', 'inputTextStd', 'date', 'inputDate' )}
+        ${generatesInputFieldHTML('label', 'input', 'Due Date', 'inputTextStd', 'date', 'inputDate')}
         ${generateLabelsHTML('label', 'Prio')}
             
            
@@ -61,15 +103,31 @@ function getCategory(i) {
     return categories[i];
 }
 
-function addOptionWithFunction(){
+function addOptionWithFunction() {
     document.getElementById('categoryBox').innerHTML += `
-            <option><a onselect="changeToInputField()">Add new Category</a></option>`;
+            <div onselect="changeToInputField()" class="cl_categories d-none">Add new Category</div>`;
 }
 
-function changeToInputField(){
-    if(document.getElementById('categoryBox').value == 'Add new Category'){
-        document.getElementById('id_categoryBox').innerHTML = generatesChangedInputFieldHTML('label', 'input', 'newCategory', 'inputTextStd', 'text','newCat');
+function cancelNewCategory(){
+    document.getElementById('id_categoryBox').innerHTML = setBackToOptionsField('label', 'select', 'Category', 'dropDownMenuField', 'categoryBox');
+    addOptionWithFunction();
+    generateOptionsHTML('categoryBox', categories, 'categories');
 
+}
+
+function setBackToOptionsField(field1, field2, headline, properties, id){
+    return `
+    <${field1}>${headline}</${field1}>
+    <${field2} onchange="changeToInputField()" class="${properties}" id="${id}" placeholder="Choose">
+    <option value="" disabled selected>Select your option</option>
+
+    </${field2}>
+    `;
+}
+
+function changeToInputField() {
+    if (document.getElementById('categoryBox').value == 'Add new Category') {
+        document.getElementById('id_categoryBox').innerHTML = generatesChangedInputFieldHTML('label', 'input', 'newCategory', 'inputTextStd', 'text', 'newCat');
     }
 }
 
@@ -89,9 +147,9 @@ function generateLabelsHTML(field, headline) {
     <div class="detail">
         <${field}>${headline}</${field}>
         <div class="d-flex">
-            <${field}>Urgent</${field}>
-            <${field}>Medium</${field}>
-            <${field}>Low</${field}>
+            <${field} id="id_urgent" onclick="changeStyleOfLabel('id_urgent')">Urgent <img src="img/urgentIcon.png" class="prioImg" id="urgentImgID"> </${field}>
+            <${field} id="id_medium" onclick="changeStyleOfLabel('id_medium')">Medium <img src="img/mediumIcon.png" class="prioImg" id="mediumImgID"> </${field}>
+            <${field} id="id_low" onclick="changeStyleOfLabel('id_low')">Low <img src="img/lowIcon.png" class="prioImg" id="lowImgID"> </${field}>
         </div>
     </div>
     `;
@@ -108,7 +166,7 @@ function generatesTextareaFieldHTML(field1, field2, headline) {
 
 
 
-function generatesInputFieldHTML(field1, field2, headline, properties, type,id) {
+function generatesInputFieldHTML(field1, field2, headline, properties, type, id) {
     return `
     <div class="detail" id="id_${id}">
         <${field1}>${headline}</${field1}>
@@ -116,39 +174,71 @@ function generatesInputFieldHTML(field1, field2, headline, properties, type,id) 
     </div>
     `;
 }
-function generatesChangedInputFieldHTML(field1, field2, headline, properties, type,id) {
+function generatesChangedInputFieldHTML(field1, field2, headline, properties, type, id) {
     return `
-        <${field1}>${headline}</${field1}>
-        <${field2} class="${properties}" type="${type}" id="${id}" >
+    <${field1}>${headline}</${field1}>
+
+    <${field2} class="${properties}" type="${type}" id="${id}" >
+    <div class="newCat">
+
+        <a onclick="cancelNewCategory()"><img src="img/cancelIcon.png"></a>
+        <div class="border-1px-solid width0-height31px"></div>
+        <a><img src="img/checkIcon.png"></a>
+    </div>  
     `;
 }
 
-function generatesOptionsFieldHTML(field1, field2, headline, properties, id) {
-    
+function generatesOptionsFieldHTML(field1, field2, headline, properties, id, usedItems) {
+
     return `
     <div class="detail" id="id_${id}">
         <${field1}>${headline}</${field1}>
-        <${field2} onchange="changeToInputField()" class="${properties}" id="${id}" placeholder="Choose">
-        <option value="" disabled selected>Select your option</option>
-
-        </${field2}>
+        <div onchange="changeToInputField()" class="${properties}" id="${id}">
+            <div onclick="showDropDownItems('${usedItems}')" class="dropDownStart" value="" disabled selected>Select task category<img src="img/dropdownIcon.png">
+            </div>
+        </div>
     </div>
     `;
+}
+
+function showDropDownItems(usedItems){
+    if(usedItems == 'categories'){
+        showCategoryItems();
+    }
+    if(usedItems == 'users'){
+        showUsersItems();
+    }
+}
+
+function showCategoryItems(){
+    for(let i = 0;i<categories.length;i++){
+        if(document.getElementById(categories[i]).classList.contains('d-none')){
+        document.getElementById(categories[i]).classList.remove('d-none');
+        }
+    }
+}
+
+function showUsersItems(){
+    for(let i = 0;i<users.length;i++){
+        if(document.getElementById(users[i]['name']).classList.contains('d-none')){
+        document.getElementById(users[i]['name']).classList.remove('d-none');
+        }
+    }
 }
 
 function generateOptionsHTML(id, array, nameOfArray) {
     for (let i = 0; i < array.length; i++) {
-        if(nameOfArray == 'users'){
+        if (nameOfArray == 'users') {
             document.getElementById(id).innerHTML += `
-            <option value="${getUser(i)}">${getUser(i)}</option>
+            <div class="cl_${nameOfArray} d-none" id="${getUser(i)}" value="${getUser(i)}">${getUser(i)}</div>
         `;
         }
-        if(nameOfArray == 'categories'){
+        if (nameOfArray == 'categories') {
             document.getElementById(id).innerHTML += `
-            <option value="${getCategory(i)}">${getCategory(i)}</option>
+            <div class="cl_${nameOfArray} d-none" id="${getCategory(i)}" value="${getCategory(i)}" >${getCategory(i)}</div>
         `;
         }
-        
+
     }
 }
 
