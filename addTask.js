41000751371ea4ad9bddf544ff1b209d1dd12318
Marijan Dupkovic <mdu@dupko-it.de>
2023-04-14 17:57:46 +0200

    Added Colorchoser

--- addTask.js
+++ addTask.js
@@ -4,21 +4,20 @@ function generateAddTaskHTML() {
     <form onsubmit="createNewTask()" class="addTaskForm">
     <div class="details">
         <div class="detailBox-left">
-            ${generatesInputFieldHTML('label', 'input', 'Title', 'inputTextStd', 'text', 'inputTitle')}
-            ${generatesTextareaFieldHTML('label', 'textarea', 'Description')}
-            ${generatesOptionsFieldHTML('label', 'select', 'Category', 'dropDownMenuField', 'categoryBox', 'categories')}
-            ${generatesOptionsFieldHTML('label', 'select', 'Assigned to', 'dropDownMenuField', 'assignedTo', 'users')}
+            ${generatesInputFieldHTML('label', 'input', 'Title', 'inputTextStd', 'text', 'inputTitle', 'Enter a title')}
+            ${generatesTextareaFieldHTML('label', 'textarea', 'Description', 'Enter a description')}
+            ${generatesOptionsFieldHTML('label', 'select', 'Category', 'dropDownMenuField', 'categoryBox', 'categories', './img/dropdownIcon.png')}
+            ${generatesOptionsFieldHTML('label', 'select', 'Assigned to', 'dropDownMenuField', 'assignedTo', 'users', './img/dropdownIcon.png')}
         </div>
         <div class="border-1px-solid"></div>
         <div class="detailBox-right">
         ${generatesInputFieldHTML('label', 'input', 'Due Date', 'inputTextStd', 'date', 'inputDate')}
         ${generateLabelsHTML('label', 'Prio')} 
-            <div class="detail">
-                <label>Subtasks</label>
-                <div class="p-relative d-flex align-c">
-                    <input type="text" placeholder="Add a new Subtask" class="inputTextStd" id="newSubtask">
-                    <img onclick="generateCheckboxItem()" src="./img/addIcon.png" class="inputAddIcon">
-                </div>
+
+        ${generateSubTaskField('label', 'Subtasks', 'dropDownMenuField', 'addNewSubTask', './img/addIcon.png')}
+
+        <div class="p-relative d-flex align-c">
+
                 <list class="" id="list-subtask">
                 </list>
             </div>
@@ -37,11 +36,48 @@ function generateAddTaskHTML() {
         </form>
     `;
     generateOptionsHTML('assignedTo', users, 'users');
-    addOptionWithFunction();
+    addOptionWithFunction('addNewCat');
     generateOptionsHTML('categoryBox', categories, 'categories');
 }
 
 function createNewTask(){
+    
+}
+
+function addColorChoser(){
+    document.getElementById('id_categoryBox').innerHTML += `
+    <div class="d-flex">
+        <a onclick="setActiveColor('8AA4FF')" id="8AA4FF" value="#8AA4FF" class="colorCircle"style="background-color:#8AA4FF;"></a>
+        <a onclick="setActiveColor('FF0000')" id="FF0000" value="#FF0000" class="colorCircle"style="background-color:#FF0000;"></a>
+        <a onclick="setActiveColor('2AD300')" id="2AD300" value="#2AD300" class="colorCircle"style="background-color:#2AD300;"></a>
+        <a onclick="setActiveColor('FF8A00')" id="FF8A00" value="#FF8A00" class="colorCircle"style="background-color:#FF8A00;"></a>
+        <a onclick="setActiveColor('E200BE')" id="E200BE" value="#E200BE" class="colorCircle"style="background-color:#E200BE;"></a>
+        <a onclick="setActiveColor('0038FF')" id="0038FF" value="#0038FF" class="colorCircle"style="background-color:#0038FF;"></a>
+        </div>
+    `;
+}
+
+function getValueOfChosenColor(){
+    for(let i = 0;i<colors.length;i++){
+        let isActive = document.getElementById(colors[i].slice(1)).classList.contains('colorCircleisActive');
+        let setValue = document.getElementById(colors[i].slice(1)).value;
+        if(isActive){
+            return setValue;
+        }
+    }
+   
+
+}
+
+function setActiveColor(id){
+    document.getElementById('8AA4FF').classList.remove('colorCircleisActive');
+    document.getElementById('FF0000').classList.remove('colorCircleisActive');
+    document.getElementById('2AD300').classList.remove('colorCircleisActive');
+    document.getElementById('FF8A00').classList.remove('colorCircleisActive');
+    document.getElementById('E200BE').classList.remove('colorCircleisActive');
+    document.getElementById('0038FF').classList.remove('colorCircleisActive');
+
+    document.getElementById(id).classList.add('colorCircleisActive');
     
 }
 
