962e1af66ea7ce588c5c2944d97647652e9e31e5
Marijan Dupkovic <mdu@dupko-it.de>
2023-05-15 16:02:29 +0200

    fixed size of dropdowns

--- js/script.js
+++ js/script.js
@@ -299,27 +299,22 @@ function checkDropDown() {
     if (!selectedCategory) {
         if (!dropDownCat) {
             document.getElementById('CategorycategoryBox').classList.remove('d-none');
-            document.getElementById('categoryBox').style = `height:unset !important;`;
             document.getElementById('addNewCat').classList.remove('d-none');
             dropDownCat = true;
         } else {
             document.getElementById('addNewCat').classList.add('d-none');
-            document.getElementById('categoryBox').style = `height:41px !important;`;
-            document.getElementById('assignedTo').style = `height:41px !important;`;
             dropDownCat = false;
         }
     } else {
         if (!dropDownCat) {
             document.getElementById('CategorycategoryBox').classList.remove('d-none');
-            document.getElementById('categoryBox').style = `height:unset !important;`;
             document.getElementById('addNewCat').classList.remove('d-none');
 
-            document.getElementById('assignedTo').style = `height:unset !important;`;
             dropDownCat = true;
         } else {
             document.getElementById('addNewCat').classList.add('d-none');
             document.getElementById('CategorycategoryBox').classList.add('d-none');
-            document.getElementById('categoryBox').style = `height:41px !important;`;
+
             dropDownCat = false;
         }
     }
@@ -357,19 +352,18 @@ function setOnSubmitForm(addForm) {
 function showUsersItems() {
     if (!dropDownAssign) {
         document.getElementById('Assigned toassignedTo').classList.remove('d-none');
-        document.getElementById('assignedTo').style = `height:unset !important;`;
+        document.getElementById('assignedTo').style = `height:unset;`;
         document.getElementById('invite').classList.remove('d-none');
         dropDownAssign = true;
     } else {
+
         if (document.getElementById('Assigned toassignedTo').classList.contains('d-none')) {
             document.getElementById('Assigned toassignedTo').classList.add('d-none');
             document.getElementById('invite').classList.remove('d-none');
 
-            document.getElementById('assignedTo').style = `height:41px !important;`;
         } else {
             document.getElementById('Assigned toassignedTo').classList.remove('d-none');
             document.getElementById('invite').classList.add('d-none');
-            document.getElementById('assignedTo').style = `height:unset !important;`;
         }
         dropDownAssign = false;
     }
@@ -380,7 +374,6 @@ function showUsersItems() {
         }
         else {
             document.getElementById(users[i]['name']).classList.add('d-none');
-            document.getElementById('assignedTo').style = `height:41px !important;`;
         }
     }
 }
