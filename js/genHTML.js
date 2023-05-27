c37e0f19ab2d01b3f9a7516c4ee5e83e8f01078d
Oural Mantenli <oural.mantenli@gmail.com>
2023-05-27 23:37:56 +0200

    add overlay

--- js/genHTML.js
+++ js/genHTML.js
@@ -585,7 +585,7 @@ function generateContactDetailsHTML() {
  */
 function addCloseBtnToAddTaskAtContacts(overlay) {
     document.getElementById('addTaskAtContacts').innerHTML += `
-    <div onclick="addDisplayNone('addTaskAtContacts')" class="closeIconAtContacts ${overlay}" id="closeIconAtContacts"></div>
+    <div onclick="addDisplayNone('addTaskAtContacts'), addDisplayNone('overlay')" class="closeIconAtContacts ${overlay}" id="closeIconAtContacts"></div>
     `;
 }
 
